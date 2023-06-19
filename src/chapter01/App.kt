2ac5722616ebb5c6c0239760162ec0d77b5c4d7d
Vasiliy Sidorenko <sidorenkovn@list.ru>
2023-06-19 22:23:36 +0300

    YabbaDabbaDo + chapter rename

--- src/chapter01/App.kt
+++ src/chapter01/App.kt
@@ -1,4 +1,4 @@
-package chapter1
+package chapter01
 
 fun main () {
     val x = 3
---
+++ src/chapter01/YabbaDabbaDo.kt
@@ -0,0 +1,11 @@
+package chapter01
+
+fun main() {
+    var x = 1
+    while(x < 3){
+        print(if (x == 1) "Yab" else "Dab")
+        print("ba")
+        x = x + 1
+    }
+    if (x == 3) println("Do")
+}
