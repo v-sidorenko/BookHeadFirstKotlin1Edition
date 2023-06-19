2797ed88b852318ee4147cbf01570fc358ee143b
Vasiliy Sidorenko <sidorenkovn@list.ru>
2023-06-19 21:58:45 +0300

    while loop

--- src/chapter1/App.kt
+++ src/chapter1/App.kt
@@ -1,5 +1,11 @@
 package chapter1
 
 fun main () {
-    println("Pow!")
+    var x = 1
+    println("Before the loop. x = $x")
+    while (x < 4){
+        println("In the loop. x = $x")
+        x = x + 1
+    }
+    println("After the loop. x = $x")
 }
