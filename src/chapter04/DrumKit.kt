7dc7c3ab3e3b99c11d527c860765a63c663d2ac8
Vasiliy Sidorenko <sidorenkovn@list.ru>
2023-07-11 22:19:28 +0300

    DrumKit

---
+++ src/chapter04/DrumKit.kt
@@ -0,0 +1,26 @@
+package chapter04
+
+class DrumKit(var hasTopHat: Boolean, var hasSnare: Boolean) {
+
+    fun playTopHat() {
+        if (hasTopHat)
+        println("ding ding ba-da-bing!")
+    }
+
+    fun playSnare(){
+        if (hasSnare)
+        println("bang bang bang!")
+    }
+}
+
+fun main(){
+    val d = DrumKit(true, true)
+
+    d.playTopHat()
+    d.playSnare()
+
+    d.hasSnare = false
+
+    d.playTopHat()
+    d.playSnare()
+}
