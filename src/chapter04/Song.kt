02270c588ce9ec37157f0925187f868b5a4c3482
Vasiliy Sidorenko <sidorenkovn@list.ru>
2023-07-11 22:06:13 +0300

    Song

---
+++ src/chapter04/Song.kt
@@ -0,0 +1,22 @@
+package chapter04
+
+class Song(val title: String, val artist: String) {
+
+    fun play(){
+        println("Playing the song $title by $artist")
+    }
+
+    fun stop(){
+        println("Stopped playing $title")
+    }
+}
+
+fun main() {
+    val songOne = Song("The Mesopotamians", "They Might Be Giants")
+    val songTwo = Song("Going Underground", "The Jam")
+    val songThree = Song("Make Me Smile", "Steve Harley")
+
+    songTwo.play()
+    songTwo.stop()
+    songThree.play()
+}
