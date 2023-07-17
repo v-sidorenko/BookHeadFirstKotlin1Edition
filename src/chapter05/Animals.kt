3956cf7b6858e2453682087807c28842268bd88a
Vasiliy Sidorenko <sidorenkovn@list.ru>
2023-07-17 22:21:02 +0300

    Animals

--- src/chapter05/Animals.kt
+++ src/chapter05/Animals.kt
@@ -56,3 +56,23 @@ class Wolf: Canine(){
         println("The Wolf is eating $food")
     }
 }
+
+class Vet {
+    fun giveShot(animal: Animal){
+        animal.makeNoise()
+    }
+}
+
+fun main(){
+    val animals = arrayOf(Hippo(), Wolf())
+    for(item in animals){
+        item.roam()
+        item.eat()
+    }
+
+    val vet = Vet()
+    val wolf = Wolf()
+    val hippo = Hippo()
+    vet.giveShot(wolf)
+    vet.giveShot(hippo)
+}
