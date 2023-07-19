8692ab6d5051bd27b6bf2ee81e0d392f42157520
Vasiliy Sidorenko <sidorenkovn@list.ru>
2023-07-19 22:05:06 +0300

    Animals abstract

---
+++ src/chapter06/Animals.kt
@@ -0,0 +1,74 @@
+package chapter06
+
+abstract class Animal{
+    abstract val image: String
+    abstract val food: String
+    abstract val habitat: String
+    var hunger = 10
+
+    abstract fun makeNoise()
+
+    abstract fun eat()
+
+    open fun roam(){
+        println("The Animal is roaming")
+    }
+
+    fun sleep(){
+        println("The Animal is sleeping")
+    }
+}
+
+class Hippo: Animal(){
+    override val image = "hippo.jpg"
+    override val food = "grass"
+    override val habitat = "water"
+
+    override fun makeNoise() {
+        println("Grunt! Grunt!")
+    }
+
+    override fun eat() {
+        println("The Hippo is eating $food")
+    }
+}
+
+abstract class Canine: Animal(){
+    override fun roam() {
+        println("The Canine is roaming")
+    }
+}
+
+class Wolf: Canine(){
+    override val image = "wolf.jpg"
+    override val food = "meat"
+    override val habitat = "forests"
+
+    override fun makeNoise() {
+        println("Hooooowl!")
+    }
+
+    override fun eat() {
+        println("The Wolf is eating $food")
+    }
+}
+
+class Vet {
+    fun giveShot(animal: Animal){
+        animal.makeNoise()
+    }
+}
+
+fun main(){
+    val animals = arrayOf(Hippo(), Wolf())
+    for(item in animals){
+        item.roam()
+        item.eat()
+    }
+
+    val vet = Vet()
+    val wolf = Wolf()
+    val hippo = Hippo()
+    vet.giveShot(wolf)
+    vet.giveShot(hippo)
+}
