15e5f55b9ca73f33fa266ad42e8e3c11262df5a3
Vasiliy Sidorenko <sidorenkovn@list.ru>
2023-07-24 23:25:14 +0300

    Recipes modification

--- src/chapter07/Recipes.kt
+++ src/chapter07/Recipes.kt
@@ -1,10 +1,35 @@
 package chapter07
 
-data class Recipe(val title: String, val isVegetarian: Boolean)
+data class Recipe(val title: String,
+                  val mainIngredient: String,
+                  val isVegetarian: Boolean= false,
+                  val difficulty: String = "Easy")
+
+class Mushroom(val size: Int, val isMagic: Boolean){
+    constructor(isMagic_param: Boolean): this(0, isMagic_param){
+        //code
+    }
+}
+
+fun findRecipe(title: String = "",
+               ingredient:String = "",
+               isVegetarian: Boolean = false,
+               difficulty: String = ""): Array<Recipe>{
+    //code
+    return arrayOf(Recipe(title, ingredient, isVegetarian, difficulty))
+}
+
+fun addNumber(a: Int, b: Int): Int{
+    return a + b
+}
+
+fun addNumber(a: Double, b: Double): Double{
+    return a + b
+}
 
 fun main(){
-    val r1 = Recipe("Thai Curry", false)
-    val r2 = Recipe("Thai Curry", false)
+    val r1 = Recipe("Thai Curry", "Chicken")
+    val r2 = Recipe(title = "Thai Curry", mainIngredient = "Chicken")
     val r3 = r1.copy(title = "Chicken Bhuna")
     println("r1 hash code: ${r1.hashCode()}")
     println("r2 hash code: ${r2.hashCode()}")
@@ -13,6 +38,14 @@ fun main(){
     println("r1 == r2? ${r1 == r2}")
     println("r1 === r2? ${r1 === r2}")
     println("r1 == r3? ${r1 == r3}")
-    val (title, vegetarian) = r1
+    val (title, mainIngredient, vegetarian, difficulty) = r1
     println("title is $title and vegetarian is $vegetarian")
+
+    val m1 = Mushroom(6, false)
+    println("m1 size is ${m1.size} and isMagic is ${m1.isMagic}")
+    val m2 = Mushroom(true)
+    println("m1 size is ${m2.size} and isMagic is ${m2.isMagic}")
+
+    println(addNumber(2, 5))
+    println(addNumber(1.6, 7.3))
 }
