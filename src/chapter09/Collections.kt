dfca32bc0fe57b6ee3c7d6fd937cadbd396f2b7b
Vasiliy Sidorenko <sidorenkovn@list.ru>
2023-11-15 22:42:56 +0300

    Collections add

---
+++ src/chapter09/Collections.kt
@@ -0,0 +1,19 @@
+package chapter09
+
+fun main(){
+    val mShoppingList = mutableListOf("Tea", "Eggs", "Milk")
+    println("mShoppingList original: $mShoppingList")
+    val extraShopping = listOf("Cookies", "Sugar", "Eggs")
+    mShoppingList.addAll(extraShopping)
+    println("mShoppingList items added: $mShoppingList")
+    if (mShoppingList.contains("Tea")){
+        mShoppingList.set(
+                mShoppingList.indexOf("Tea"),
+                "Coffee"
+        )
+    }
+    mShoppingList.sort()
+    println("mShoppingList sorted: $mShoppingList")
+    mShoppingList.reverse()
+    println("mShoppingList reversed: $mShoppingList")
+}
