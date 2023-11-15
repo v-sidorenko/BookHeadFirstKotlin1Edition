c1fe58f93bbd839f6014696d01ff8e7d1ff1e86f
Vasiliy Sidorenko <sidorenkovn@list.ru>
2023-11-15 22:52:43 +0300

    CodeMagnets add

---
+++ src/chapter09/CodeMagnets.kt
@@ -0,0 +1,22 @@
+package chapter09
+
+fun main(){
+
+    var a: MutableList<String> = mutableListOf()
+
+    a.add(0, "Zero")
+    a.add(1, "Two")
+    a.add(2, "Four")
+    a.add(3, "Six")
+    println(a)
+
+    if (a.contains("Zero")) a.add("Eight")
+    a.removeAt(0)
+    println(a)
+
+    if (a.indexOf("Four") != 4) a.add("Ten")
+    println(a)
+
+    if (a.contains("Zero")) a.add("Twelve")
+    println(a)
+}
