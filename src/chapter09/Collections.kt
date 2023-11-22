08a6a67d7886303a45e569e5bccfe5c81f10ffb1
Vasiliy Sidorenko <sidorenkovn@list.ru>
2023-11-22 22:54:36 +0300

    update

--- src/chapter09/Collections.kt
+++ src/chapter09/Collections.kt
@@ -1,7 +1,7 @@
 package chapter09
 
 fun main(){
-    val mShoppingList = mutableListOf("Tea", "Eggs", "Milk")
+    var mShoppingList = mutableListOf("Tea", "Eggs", "Milk")
     println("mShoppingList original: $mShoppingList")
     val extraShopping = listOf("Cookies", "Sugar", "Eggs")
     mShoppingList.addAll(extraShopping)
@@ -16,4 +16,12 @@ fun main(){
     println("mShoppingList sorted: $mShoppingList")
     mShoppingList.reverse()
     println("mShoppingList reversed: $mShoppingList")
+
+    val mShoppingSet = mShoppingList.toMutableSet()
+    println("mShoppingSet: $mShoppingSet")
+    val moreShopping = setOf("Chives", "Spinach", "Milk")
+    mShoppingSet.addAll(moreShopping)
+    println("mShoppingSet items added: $mShoppingSet")
+    mShoppingList = mShoppingSet.toMutableList()
+    println("mShoppingList new version: $mShoppingList")
 }
