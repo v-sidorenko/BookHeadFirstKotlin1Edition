1744942d5873608ca377e8741821eb2754176f88
Vasiliy Sidorenko <sidorenkovn@list.ru>
2023-11-26 20:58:31 +0300

    update

--- src/chapter09/Collections.kt
+++ src/chapter09/Collections.kt
@@ -1,5 +1,6 @@
 package chapter09
 
+data class Recipe(var name: String)
 fun main(){
     var mShoppingList = mutableListOf("Tea", "Eggs", "Milk")
     println("mShoppingList original: $mShoppingList")
@@ -24,4 +25,18 @@ fun main(){
     println("mShoppingSet items added: $mShoppingSet")
     mShoppingList = mShoppingSet.toMutableList()
     println("mShoppingList new version: $mShoppingList")
+
+    val r1 = Recipe("Chicken Soup")
+    val r2 = Recipe("Quinoa Salad")
+    val r3 = Recipe("Thai Curry")
+    val r4 = Recipe("Jambalaya")
+    val r5 = Recipe("Sausage Rolls")
+    val mRecipeMap = mutableMapOf("Recipe 1" to r1, "Recipe 2" to r2, "Recipe 3" to r3)
+    println("mRecipeMap original: $mRecipeMap")
+    val recipesToAdd = mapOf("Recipe 4" to r4, "Recipe 5" to r5)
+    mRecipeMap.putAll(recipesToAdd)
+    println("mRecipeMap updated: $mRecipeMap")
+    if (mRecipeMap.containsKey("Recipe 1")){
+        println("Recipe 1 is: ${mRecipeMap.getValue("Recipe 1")}")
+    }
 }
