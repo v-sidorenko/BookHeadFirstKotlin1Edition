415f9c3886ff9803a42135d44caecfb9e01364d2
Vasiliy Sidorenko <sidorenkovn@list.ru>
2023-12-04 22:36:17 +0300

    Converters add

---
+++ src/chapter11/Converters.kt
@@ -0,0 +1,19 @@
+package chapter11
+
+fun convert(x: Double,
+            converter: (Double) -> Double) : Double {
+    val result = converter(x)
+    println("$x is converted to $result")
+    return result
+}
+
+fun convertFive(converter: (Int) -> Double) : Double {
+    val result = converter(5)
+    println("5 is converted to $result")
+    return result
+}
+
+fun main() {
+    convert(20.0) { it * 1.8 + 32}
+    convertFive { it * 1.8 + 32 }
+}
