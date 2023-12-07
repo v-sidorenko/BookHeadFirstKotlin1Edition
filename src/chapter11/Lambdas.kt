fa951a28c05d760437a7c26c53564f2e8e50ddc3
Vasiliy Sidorenko <sidorenkovn@list.ru>
2023-12-07 22:44:50 +0300

    Lambdas update

--- src/chapter11/Lambdas.kt
+++ src/chapter11/Lambdas.kt
@@ -1,5 +1,24 @@
 package chapter11
 
+typealias DoubleConversion = (Double) -> Double
+
+fun getConversionLambda(str: String): DoubleConversion{
+    if (str == "CentigradeToFahrenheit") {
+        return { it * 1.8 + 32 }
+    } else if (str == "KgsToPounds") {
+        return { it * 2.204623 }
+    } else if (str == "PoundToUSTons") {
+        return { it / 2000 }
+    } else {
+        return { it }
+    }
+}
+
+fun combine(lambda1: DoubleConversion,
+            lambda2: DoubleConversion) : DoubleConversion {
+    return { x -> lambda2(lambda1(x)) }
+}
+
 fun main(){
     var addFive = { x: Int -> x + 5 }
     println("Pass 6 to addFive: ${addFive(6)}")
@@ -16,4 +35,12 @@ fun main(){
 
     val myLambda: () -> Unit = { println("Hi!") }
     myLambda()
+
+    println("Convert 2.5kg to Pounds: ${getConversionLambda("KgsToPounds") (2.5)}")
+
+    val kgsToPoundsLambda = getConversionLambda("KgsToPounds")
+    val poundsToUSTonsLambda = getConversionLambda("PoundToUSTons")
+    val kgsToUSTonsLambda = combine(kgsToPoundsLambda, poundsToUSTonsLambda)
+    val value = 17.4
+    println("$value kgs is ${convert(value, kgsToUSTonsLambda)} US tons")
 }
