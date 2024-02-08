c2fde7a72b238ce302e50748c2b3a32768e65f76
Vasiliy Sidorenko <sidorenkovn@list.ru>
2024-02-08 23:18:56 +0300

    add Beats

---
+++ src/AppendixA/Beats.kt
@@ -0,0 +1,36 @@
+package AppendixA
+
+import java.io.File
+import javax.sound.sampled.AudioSystem
+import kotlin.io.path.Path
+
+fun playBeats (beats: String, file: String) {
+    val parts = beats.split("x")
+    var count = 0
+    for (part in parts) {
+        count += part.length + 1
+        if (part == "") {
+            playSound (file)
+        } else {
+            Thread.sleep(100 * (part.length + 1L))
+            if (count < beats.length) {
+                playSound (file)
+            }
+        }
+    }
+}
+
+fun playSound (file: String) {
+    val clip = AudioSystem.getClip()
+    val audioInputStream = AudioSystem.getAudioInputStream(
+            File(file)
+    )
+    clip.open(audioInputStream)
+    clip.start()
+}
+
+fun main () {
+
+    playBeats("x-x-x-x-x-x-", "src/AppendixA/audio/toms.aiff")
+    playBeats("x-----x-----", "src/AppendixA/audio/crash_cymbal.aiff")
+}
