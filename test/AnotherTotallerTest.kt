a65be50b0cd7526ce89f22deb2f15ad370f2ffcc
Vasiliy Sidorenko <sidorenkovn@list.ru>
2024-03-12 23:23:21 +0300

    add another kotest test

--- test/AnotherTotallerTest.kt
+++ test/AnotherTotallerTest.kt
@@ -1,5 +1,7 @@
 import appendixB.Totaller
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class AnotherTotallerTest: StringSpec ({
@@ -9,5 +11,16 @@ class AnotherTotallerTest: StringSpec ({
         totaller.add(3) shouldBe 3
         totaller.add(4) shouldBe 7
         totaller.total shouldBe 7
+    }
+
+    "should be able to add all lots of different numbers" {
+        forAll(
+            row(1, 2, 3),
+            row(19, 47, 66),
+            row(11, 21, 32),
+        ) { x, y, expectedTotal ->
+            val totaller = Totaller(x)
+            totaller.add(y) shouldBe expectedTotal
+        }
     }
 })
